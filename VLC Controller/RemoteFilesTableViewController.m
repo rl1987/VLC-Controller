b574ac65fbda538cf36731dc3aad7084b9ce9dca
rl1987 <rl1987@sdf.lonestar.org>
2017-03-13 18:27:03 +0200

    Implement directory browsing

--- VLC Controller/RemoteFilesTableViewController.m
+++ VLC Controller/RemoteFilesTableViewController.m
@@ -8,96 +8,93 @@
 
 #import "RemoteFilesTableViewController.h"
 
+#import "PlayerManager.h"
+#import "RemoteFile.h"
+
 @interface RemoteFilesTableViewController ()
+
+@property (nonatomic, strong) NSArray<RemoteFile *> *remoteFiles;
+
+@property (nonatomic, strong) NSString *uri;
 
 @end
 
 @implementation RemoteFilesTableViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self reloadFileList];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.remoteFiles.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"file_cell" forIndexPath:indexPath];
     
     // Configure the cell...
+    RemoteFile *remoteFile = self.remoteFiles[indexPath.row];
+    
+    cell.textLabel.text = remoteFile.fileName;
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    RemoteFile *remoteFile = self.remoteFiles[indexPath.row];
+    
+    if (remoteFile.isDirectory) {
+        self.uri = remoteFile.uri;
+        [self reloadFileList];
+    } else {
+        // TODO
+    }
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)doneTapped
 {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
+}
+
+#pragma mark - Private routines
+
+- (void)reloadFileList
+{
+    if (self.uri)
+    {
+        [[PlayerManager defaultManager] listRemoteFilesAtURI:self.uri
+                                       withCompletionHandler:^(NSArray<RemoteFile *> *files, NSError *error) {
+                                           if (!error) {
+                                               self.remoteFiles = files;
+                                               [self.tableView reloadData];
+                                           }
+                                       }];
+    }
+    else
+    {
+        [[PlayerManager defaultManager] listRemoteFilesInDirectory:@"."
+                                             withCompletionHandler:^(NSArray<RemoteFile *> *files, NSError *error) {
+                                                 if (!error) {
+                                                     self.remoteFiles = files;
+                                                     [self.tableView reloadData];
+                                                 }
+                                             }];
+    }
 }
 
 @end
