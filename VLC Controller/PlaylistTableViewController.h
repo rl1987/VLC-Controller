63a0e948d6e64933b1b9ea970dfa4b1debf218f9
rl1987 <rl1987@sdf.lonestar.org>
2017-03-14 15:05:34 +0200

    New storyboard scene for playlist

---
+++ VLC Controller/PlaylistTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlaylistTableViewController.h
+//  VLC Controller
+//
+//  Created by rl1987 on 14/03/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistTableViewController : UITableViewController
+
+- (IBAction)doneTapped;
+
+@end
